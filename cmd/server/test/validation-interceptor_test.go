a7e58666d22e1d5b4a2da640129e432bab9658ed
Stefan Majer <stefan.majer@f-i-ts.de>
2025-01-26 13:52:57 +0100

    Move validation test

---
+++ cmd/server/test/validation-interceptor_test.go
@@ -0,0 +1,49 @@
+package test
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"connectrpc.com/connect"
+	"connectrpc.com/validate"
+	apiv1 "github.com/metal-stack/api/go/api/v1"
+	"github.com/metal-stack/api/go/api/v1/apiv1connect"
+	"github.com/stretchr/testify/require"
+)
+
+func TestWithValidator(t *testing.T) {
+	t.Parallel()
+	interceptor, err := validate.NewInterceptor()
+	require.NoError(t, err)
+
+	mux := http.NewServeMux()
+	mux.Handle(apiv1connect.TokenServiceCreateProcedure, connect.NewUnaryHandler(
+		apiv1connect.TokenServiceCreateProcedure,
+		createToken,
+		connect.WithInterceptors(interceptor),
+	))
+	srv := startHTTPServer(t, mux)
+
+	req := connect.NewRequest(&apiv1.TokenServiceCreateRequest{
+		Description: "",
+	})
+	_, err = apiv1connect.NewTokenServiceClient(srv.Client(), srv.URL).Create(context.Background(), req)
+	require.Error(t, err)
+	require.EqualError(t, err, "invalid_argument: validation error:\n - description: value length must be at least 2 characters [string.min_len]")
+	require.Equal(t, connect.CodeInvalidArgument, connect.CodeOf(err))
+}
+
+func startHTTPServer(tb testing.TB, h http.Handler) *httptest.Server {
+	tb.Helper()
+	srv := httptest.NewUnstartedServer(h)
+	srv.EnableHTTP2 = true
+	srv.Start()
+	tb.Cleanup(srv.Close)
+	return srv
+}
+
+func createToken(_ context.Context, req *connect.Request[apiv1.TokenServiceCreateRequest]) (*connect.Response[apiv1.TokenServiceCreateResponse], error) {
+	return connect.NewResponse(&apiv1.TokenServiceCreateResponse{Token: &apiv1.Token{Uuid: "abc"}}), nil
+}
