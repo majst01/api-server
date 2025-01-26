df84f3671b780310756b9adfcb62519f3bca9e09
Stefan Majer <stefan.majer@f-i-ts.de>
2025-01-26 14:14:57 +0100

    Small fix

--- pkg/service/ip/ip-service.go
+++ pkg/service/ip/ip-service.go
@@ -38,7 +38,11 @@ func (i *ipServiceServer) Get(ctx context.Context, rq *connect.Request[apiv1.IPS
 	i.log.Debug("get", "ip", rq)
 	req := rq.Msg
 
-	i.m.IP().FindIP(ip.NewFindIPParamsWithContext(ctx).WithID(req.Uuid), nil)
+	// FIXME: use get here
+	_, err := i.m.IP().FindIP(ip.NewFindIPParamsWithContext(ctx).WithID(req.Uuid), nil)
+	if err != nil {
+		return nil, err
+	}
 	ip, err := i.get(req.Uuid)
 	if err != nil {
 		return nil, err
